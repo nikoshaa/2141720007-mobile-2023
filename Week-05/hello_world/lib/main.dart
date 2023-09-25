dd35b311e05789fa0994588415bf269edb8352e4
nikoshaa <2141720007@student.polinema.ac.id>
2023-09-25 16:51:26 +0700

    feat: week 5

---
+++ Week-05/hello_world/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/basic_widgets/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Wildan Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 58, 183, 143)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Wildan Hafidz'),
+    );
+  }
+}
