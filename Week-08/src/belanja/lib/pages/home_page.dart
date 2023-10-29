74ad375586b9f7be8ee52f1b3b500939831fd9e0
nikoshaa <2141720007@student.polinema.ac.id>
2023-10-29 20:28:29 +0700

    feat: add decomposition widget

--- Week-08/src/belanja/lib/pages/home_page.dart
+++ Week-08/src/belanja/lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:belanja/models/item.dart';
 import 'package:flutter/material.dart';
+import 'package:belanja/widgets/card_widget.dart';
+import 'package:belanja/widgets/bottom_widget.dart';
 
 class HomePage extends StatelessWidget {
   final List<Item> items = [
@@ -44,112 +46,10 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Shopping List'),
-      ),
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 0.7,
+        appBar: AppBar(
+          title: const Text('Shopping List'),
         ),
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          final item = items[index];
-          return InkWell(
-            onTap: () {
-              Navigator.pushNamed(context, '/item', arguments: item);
-            },
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Hero(
-                      tag: 'productImage${item.name}',
-                      child: AspectRatio(
-                        aspectRatio: 1,
-                        child: Image.asset(item.imageUrl, fit: BoxFit.cover),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8),
-                          child: Text(
-                            item.name,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            const Icon(Icons.star, color: Colors.amber),
-                            Text(
-                              item.rating.toString(),
-                              style: const TextStyle(
-                                color: Colors.amber,
-                                fontSize: 14,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8),
-                      child: Text(
-                        'Rp. ${item.price}',
-                        style: const TextStyle(
-                          color: Colors.deepOrange,
-                          fontSize: 14,
-                        ),
-                      ),
-                    ),
-                    Text(
-                      'Terjual: ${item.sold}',
-                      style: const TextStyle(
-                        color: Colors.grey,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          padding: const EdgeInsets.all(16),
-          decoration: const BoxDecoration(
-            color: Colors.blue,
-          ),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Wildan Hafidz Mauludin',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                ),
-              ),
-              Text(
-                '2141720007',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+        body: CardWidget(items: items),
+        bottomNavigationBar: const BottomWidget());
   }
 }
